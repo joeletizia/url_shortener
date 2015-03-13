8fc7803e50006aeb23f2930b4f1e9aff6caea487
Joe Letizia <joe@gust.com>
2015-03-13 17:24:12 -0400

    Add host and port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  HOST = "dry-gorge-5519.herokuapp.com"
+  PORT = "80"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
