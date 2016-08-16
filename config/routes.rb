95ad479f7881c1704ed566fa4757debc02d0c961
Joe Letizia <joe.letizia@gmail.com>
2016-08-16 10:14:11 -0400

    removed stuff necessary for interview

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-
-  resources :shortened_urls, only: [:index, :new, :create]
-
-  get '/:id', to: 'redirection#show', as: 'redirection'
 end
